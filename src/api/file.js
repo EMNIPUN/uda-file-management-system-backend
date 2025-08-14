1e9a5724e3261084ba628bf573b76242b85a342f
Nipun Dhananjaya <156228527+EMNIPUN@users.noreply.github.com>
2025-08-14 22:27:48 +0530

    fixed routes point

--- src/api/file.js
+++ src/api/file.js
@@ -1,11 +1,11 @@
 import { getAllFiles, getFileById, createFile } from '../application/file.js';
 
 export default async function handler(req, res) {
-    if (req.method === 'GET' && req.url.startsWith('/getallfile')) {
+    if (req.method === 'GET' && req.url.startsWith('/api/file/getallfile')) {
         await getAllFiles(req, res);
-    } else if (req.method === 'GET' && req.url.startsWith('/getfile/')) {
+    } else if (req.method === 'GET' && req.url.startsWith('/api/file/getfile/:id')) {
         await getFileById(req, res);
-    } else if (req.method === 'POST' && req.url.startsWith('/createfile')) {
+    } else if (req.method === 'POST' && req.url.startsWith('/api/file/createfile')) {
         await createFile(req, res);
     } else {
         res.status(404).json({ message: 'Not found' });
