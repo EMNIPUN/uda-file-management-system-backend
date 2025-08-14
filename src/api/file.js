8148dd1d51367c452ef71b57b137d138086131cd
Nipun Dhananjaya <156228527+EMNIPUN@users.noreply.github.com>
2025-08-14 22:31:18 +0530

    fixed error

--- src/api/file.js
+++ src/api/file.js
@@ -1,13 +1,14 @@
-import { getAllFiles, getFileById, createFile } from '../application/file.js';
+import express from 'express'
+import {
+    getAllFiles,
+    getFileById,
+    createFile
+} from '../application/File.js'
 
-export default async function handler(req, res) {
-    if (req.method === 'GET' && req.url.startsWith('/api/file/getallfile')) {
-        await getAllFiles(req, res);
-    } else if (req.method === 'GET' && req.url.startsWith('/api/file/getfile/:id')) {
-        await getFileById(req, res);
-    } else if (req.method === 'POST' && req.url.startsWith('/api/file/createfile')) {
-        await createFile(req, res);
-    } else {
-        res.status(404).json({ message: 'Not found' });
-    }
-}
+const fileRouter = express.Router()
+
+fileRouter.get('/getallfile', getAllFiles)
+fileRouter.get('/getfile/:id', getFileById)
+fileRouter.post('/createfile', createFile)
+
+export default fileRouter
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import fileRouter from "./api/file.js";
 const app = express();
 app.use(express.json());
 
-app.use("/", fileRouter);
+app.use("/api/files", fileRouter);
 
 connectDB();
-const PORT = process.env.PORT || 8000;  
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
