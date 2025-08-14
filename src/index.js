2d4d708160c9d73b1c892ea712e397c8165c9189
Nipun Dhananjaya <156228527+EMNIPUN@users.noreply.github.com>
2025-08-14 20:15:10 +0530

    fixed error

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ app.use(express.json());
 
 app.use("/api/files", fileRouter);
 
-
 connectDB();
-
-export default app;
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
